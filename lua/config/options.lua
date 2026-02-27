0fbb981e1de2d8614c383b484e28b57a9f1e0212
arhaan <arhaankaushal@outlook.com>
2026-02-27 11:10:25 +0000

    changed some indentation options.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.opt.winborder = 'rounded'
 vim.opt.completeopt = {"menu","menuone","noselect"}
+vim.opt.tabstop = 3
+vim.opt.smartindent = true
+vim.opt.shiftwidth = 3
+vim.opt.expandtab = true
